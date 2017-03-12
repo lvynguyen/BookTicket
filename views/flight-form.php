1d7f41c0ebb8e771d5e19a992e2298e75e82bbc0
lvynguyen <lvyngg29@gmail.com>
2017-03-12 14:12:10 +0700

    update flight-form

--- views/flight-form.php
+++ views/flight-form.php
@@ -1,111 +1,114 @@
-<div class="row">
-    <ul class="row pp-tab">
-        <li class="tab-item active">
-            <a href="">Ngày</a>
-        </li>
-        <li class="tab-item active">
-            <a href="?page=flight">Chuyến bay</a>
-        </li>
-        <li class="tab-item">
-            <a href="?page=passenger">Hành khách</a>
-        </li>
-        <li class="tab-item">
-            <a href="#">Dịch vụ</a>
-        </li>
-        <li class="tab-item">
-            <a href="#">Thanh toán</a>
-        </li>
-        <li class="tab-item">
-            <a href="#">Xác nhận</a>
-        </li>
-        <li class="tab-item">
-            <a href="#">Hoàn thành</a>
-        </li>
-    </ul>
+<div class="to-flight">
+    <div class="row">
+        <ul class="row pp-tab">
+            <li class="tab-item active">
+                <a href="">Ngày</a>
+            </li>
+            <li class="tab-item active">
+                <a href="?page=flight">Chuyến bay</a>
+            </li>
+            <li class="tab-item">
+                <a href="?page=passenger">Hành khách</a>
+            </li>
+            <li class="tab-item">
+                <a href="#">Dịch vụ</a>
+            </li>
+            <li class="tab-item">
+                <a href="#">Thanh toán</a>
+            </li>
+            <li class="tab-item">
+                <a href="#">Xác nhận</a>
+            </li>
+            <li class="tab-item">
+                <a href="#">Hoàn thành</a>
+            </li>
+        </ul>
+    </div>
+
+    <form class="row" method="post" action="">
+        <h2>Khởi hành</h2>
+        <table class="table table-striped">
+            <thead>
+            <tr>
+                <th>Ngày</th>
+                <th>Bay từ</th>
+                <th>Bay đến</th>
+                <th>Eco</th>
+                <th>Sky Boss</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>sss</td>
+                <td>ssss</td>
+                <td>dddd</td>
+                <td><input type="radio" class="radio-inline" name="to-radio" />dddd</td>
+                <td><input type="radio" class="radio-inline" name="to-radio" />dddd</td>
+            </tr>
+            <tr>
+                <td>sss</td>
+                <td>ssss</td>
+                <td>dddd</td>
+                <td><input type="radio" class="radio-inline" name="to-radio" />dddd</td>
+                <td><input type="radio" class="radio-inline" name="to-radio" />dddd</td>
+            </tr>
+            <tr>
+                <td>sss</td>
+                <td>ssss</td>
+                <td>dddd</td>
+                <td><input type="radio" class="radio-inline" name="to-radio" />dddd</td>
+                <td><input type="radio" class="radio-inline" name="to-radio" />dddd</td>
+            </tr>
+            </tbody>
+        </table>
+    </form>
+
+    <form class="row" method="post" action="">
+        <h2>Trở về</h2>
+        <table class="table table-striped">
+            <thead>
+            <tr>
+                <th>Ngày</th>
+                <th>Bay từ</th>
+                <th>Bay đến</th>
+                <th>aaaa</th>
+                <th>Giá tiền</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>sss</td>
+                <td>ssss</td>
+                <td>dddd</td>
+                <td>dddd</td>
+                <td><input type="radio" class="radio-inline" name="to-radio" />dddd</td>
+            </tr>
+            <tr>
+                <td>sss</td>
+                <td>ssss</td>
+                <td>dddd</td>
+                <td>dddd</td>
+                <td><input type="radio" class="radio-inline" name="to-radio" />dddd</td>
+            </tr>
+            <tr>
+                <td>sss</td>
+                <td>ssss</td>
+                <td>dddd</td>
+                <td>dddd</td>
+                <td><input type="radio" class="radio-inline" name="to-radio" />dddd</td>
+            </tr>
+            </tbody>
+        </table>
+
+        <div class="row form-group">
+            <a class="to-form-back" href="javascript:history.back()">
+                <i class="fa fa-arrow-circle-left"></i>
+            </a>
+            <a class="to-form-back pull-right" href="?page=passenger">
+                <i class="fa fa-arrow-circle-right"></i>
+            </a>
+        </div>
+    </form>
 </div>
 
-<form class="row" method="post" action="">
-    <h2>Khởi hành</h2>
-    <table class="table table-striped">
-        <thead>
-        <tr>
-            <th>Ngày</th>
-            <th>Bay từ</th>
-            <th> Bayđến</th>
-            <th>aaaa</th>
-            <th>aaaa</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr>
-            <td>sss</td>
-            <td>ssss</td>
-            <td>dddd</td>
-            <td>dddd</td>
-            <td>dddd</td>
-        </tr>
-        <tr>
-            <td>sss</td>
-            <td>ssss</td>
-            <td>dddd</td>
-            <td>dddd</td>
-            <td>dddd</td>
-        </tr>
-        <tr>
-            <td>sss</td>
-            <td>ssss</td>
-            <td>dddd</td>
-            <td>dddd</td>
-            <td>dddd</td>
-        </tr>
-        </tbody>
-    </table>
-</form>
-
-<form class="row" method="post" action="">
-    <h2>Trở về</h2>
-    <table class="table table-striped">
-        <thead>
-        <tr>
-            <th>Ngày</th>
-            <th>Bay từ</th>
-            <th>Bay đến</th>
-            <th>aaaa</th>
-            <th>Giá tiền</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr>
-            <td>sss</td>
-            <td>ssss</td>
-            <td>dddd</td>
-            <td>dddd</td>
-            <td><input type="radio" class="radio-inline" name="to-radio" />dddd</td>
-        </tr>
-        <tr>
-            <td>sss</td>
-            <td>ssss</td>
-            <td>dddd</td>
-            <td>dddd</td>
-            <td><input type="radio" class="radio-inline" name="to-radio" />dddd</td>
-        </tr>
-        <tr>
-            <td>sss</td>
-            <td>ssss</td>
-            <td>dddd</td>
-            <td>dddd</td>
-            <td><input type="radio" class="radio-inline" name="to-radio" />dddd</td>
-        </tr>
-        </tbody>
-    </table>
-
-    <div class="row form-group">
-        <a class="to-form-back" href="javascript:history.back()">
-            <i class="fa fa-arrow-circle-left"></i>
-        </a>
-        <a class="to-form-back pull-right" href="?page=passenger">
-            <i class="fa fa-arrow-circle-right"></i>
-        </a>
-    </div>
-</form>
 
